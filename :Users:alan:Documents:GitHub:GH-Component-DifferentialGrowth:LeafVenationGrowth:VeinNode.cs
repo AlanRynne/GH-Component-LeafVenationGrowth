893148f0fe6573320451ecf2dc124cc399dd47dc
Alan Rynne <alanrynnevidal@me.com>
2018-06-21 00:05:25 +0200

    First Commit
    Half implemented - NOT YET WORKING

---
+++ :Users:alan:Documents:GitHub:GH-Component-DifferentialGrowth:LeafVenationGrowth:VeinNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace LeafVenationGrowth
+{
+    public class VeinNode 
+    {
+        public VeinNode()
+        {
+            
+        }
+
+    }
+}
